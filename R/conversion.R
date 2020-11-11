9a20955b4643d303349dc1855f67a1f88ae28821
EvaMaeRey <evangeline.mae@gmail.com>
2020-11-11 06:37:24 -0500

    slides to gif

--- R/conversion.R
+++ R/conversion.R
@@ -1,20 +1,20 @@
 
 
 
-slides_pdf <- function(url){
+slides_pdf <- function(input){
 
-pagedown::chrome_print(input = url)
+pagedown::chrome_print(input = input)
 
 }
 
-pdf_gif <- function(path,
-                    output = paste0(gsub('pdf$', '', path), "gif"),
+pdf_gif <- function(input,
+                    output = paste0(gsub('pdf$', '', input), "gif"),
                     density = 100,
                     delay = NULL
                     ){
 
 # then create gif as follows
-images <- magick::image_read_pdf(path = path, density = density) # create images
+images <- magick::image_read_pdf(path = input, density = density) # create images
 
 if (is.null(delay)) {
 
@@ -25,3 +25,21 @@ delay <- (1/(1:length(images))) + .1
 magick::image_write_gif(images, path = output, delay = delay) # images to gif
 
 }
+
+
+slides_gif <- function(input,
+           output = paste0(gsub('html$', '', input), "gif"),
+           density = 100,
+           delay = NULL){
+
+
+pdf <- slides_pdf()
+
+pdf_gif(input = pdf,
+        output = output,
+        density = density,
+        delay = delay)
+
+
+}
+
