d1525310f7bc1e5493b42472f5cbf1f10e18e4fd
EvaMaeRey <evangeline.mae@gmail.com>
2020-11-11 06:43:38 -0500

    input fix

--- R/conversion.R
+++ R/conversion.R
@@ -27,13 +27,24 @@ magick::image_write_gif(images, path = output, delay = delay) # images to gif
 }
 
 
+#' Title
+#'
+#' @param input name of the html slides file
+#' @param output name of the gif
+#' @param density resolution maybe
+#' @param delay speed of frames
+#'
+#' @return
+#' @export
+#'
+#' @examples
 slides_gif <- function(input,
            output = paste0(gsub('html$', '', input), "gif"),
            density = 100,
            delay = NULL){
 
 
-pdf <- slides_pdf()
+pdf <- slides_pdf(input = input)
 
 pdf_gif(input = pdf,
         output = output,
