80eefc1e417fe21dc7501f14f16614b0749562bf
EvaMaeRey <evangeline.mae@gmail.com>
2020-12-11 22:10:35 -0500

    two inputs

--- R/conversion.R
+++ R/conversion.R
@@ -3,9 +3,15 @@
 
 slides_pdf <- function(input){
 
-pagedown::chrome_print(input = input)
+  for (i in 1:length(input)){
+pagedown::chrome_print(input = input[i])
+  }
+
+  pdftools::pdf_combine(input = stringr::str_replace(input, ".html", ".pdf"),
+                        output = "slides_combined.pdf")
 
 }
+
 
 pdf_gif <- function(input,
                     output = paste0(gsub('pdf$', '', input), "gif"),
@@ -22,6 +28,22 @@ magick::image_write_gif(images, path = output, delay = delay) # images to gif
 }
 
 
+pdf_video <- function(input,
+                    output = paste0(gsub('pdf$', '', input), ".mp4"),
+                    density = 200,
+                    delay = .5
+){
+
+  # then create gif as follows
+  images <- magick::image_read_pdf(path = input, density = density) # create images
+
+
+  magick::image_write_video(images, path = output, delay = delay) # images to gif
+
+}
+
+
+
 #' Title
 #'
 #' @param input name of the html slides file
@@ -33,19 +55,40 @@ magick::image_write_gif(images, path = output, delay = delay) # images to gif
 #' @export
 #'
 #' @examples
+#'
 slides_gif <- function(input,
            output = paste0(gsub('html$', '', input), "gif"),
            density = 200,
            delay = .5){
 
+slides_pdf(input = input)
 
-pdf <- slides_pdf(input = input)
-
-pdf_gif(input = pdf,
+pdf_gif(input = "slides_combined.pdf",
         output = output,
         density = density,
         delay = delay)
 
+}
+
+
+
+
+slides_video <- function(input,
+                       output = paste0(gsub('html$', '', input), "mp4"),
+                       density = 200,
+                       delay = .5){
+
+
+  pdf <- slides_pdf(input = input)
+
+  pdf_video(input = pdf,
+          output = output,
+          density = density,
+          delay = delay)
+
 
 }
+
+
+
 
