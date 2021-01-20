3b0e6d48bcad10656361b01b9ed2e4b334d64d3b
EvaMaeRey <evangeline.mae@gmail.com>
2021-01-20 18:53:39 -0500

    hello

---
+++ R/conversion (1).R
@@ -0,0 +1,87 @@
+
+
+
+slides_pdf <- function(input){
+
+pagedown::chrome_print(input = input)
+
+}
+
+pdf_gif <- function(input,
+                    output = paste0(gsub('pdf$', '', input), "gif"),
+                    density = 200,
+                    delay = .5
+                    ){
+
+# then create gif as follows
+images <- magick::image_read_pdf(path = input, density = density) # create images
+
+
+magick::image_write_gif(images, path = output, delay = delay) # images to gif
+
+}
+
+
+pdf_video <- function(input,
+                    output = paste0(gsub('pdf$', '', input), ".mp4"),
+                    density = 200,
+                    delay = .5
+){
+
+  # then create gif as follows
+  images <- magick::image_read_pdf(path = input, density = density) # create images
+
+
+  magick::image_write_video(images, path = output, delay = delay) # images to gif
+
+}
+
+
+
+#' Title
+#'
+#' @param input name of the html slides file
+#' @param output name of the gif
+#' @param density resolution maybe
+#' @param delay time frame is displayed in seconds
+#'
+#' @return a pdf and a gif
+#' @export
+#'
+#' @examples
+slides_gif <- function(input,
+           output = paste0(gsub('html$', '', input), "gif"),
+           density = 200,
+           delay = .5){
+
+
+pdf <- slides_pdf(input = input)
+
+pdf_gif(input = pdf,
+        output = output,
+        density = density,
+        delay = delay)
+
+
+}
+
+
+slides_video <- function(input,
+                       output = paste0(gsub('html$', '', input), "mp4"),
+                       density = 200,
+                       delay = .5){
+
+
+  pdf <- slides_pdf(input = input)
+
+  pdf_video(input = pdf,
+          output = output,
+          density = density,
+          delay = delay)
+
+
+}
+
+
+
+
