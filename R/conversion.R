9c520e40cb4259b88464a306aa15d39fae74d1b9
Bryan Shalloway <30487784+brshallo@users.noreply.github.com>
2021-01-25 17:25:29 -0500

    Revert "Add slides_pptx() + delete duplicated files"

--- R/conversion.R
+++ R/conversion.R
@@ -92,33 +92,5 @@ slides_video <- function(input,
 }
 
 
-#' Print Flipbook to Powerpoint
-#'
-#' @param input Name of the Rmd or html slides file.
-#' @return A .pdf, .html, series of .png images, and a .pptx
-#' @export
-#'
-slides_pptx <- function(input){
 
-  filepath_no_ext <- fs::path_ext_remove(input)
-  filepath_pdf <- fs::path_ext_set(filepath_no_ext, "pdf")
-  filepath_pptx <- fs::path_ext_set(filepath_no_ext, "pptx")
-
-  pagedown::chrome_print(input) # run if have not already created pdf
-
-  screen_imgs <- pdftools::pdf_convert(pdf = filepath_pdf, format = "png", verbose = FALSE)
-
-  doc <- officer::read_pptx()
-  for (i in 1:length(input)) {
-    doc <- doc %>%
-      officer::add_slide(layout = "Blank", master = "Office Theme") %>%
-      officer::ph_with(
-        value = officer::external_img(screen_imgs[[i]]),
-        location = officer::ph_location_fullsize()
-      )
-  }
-
-  print(doc, filepath_pptx)
-  message("You may want to edit 'ratio' in YAML header of input.")
-}
 
